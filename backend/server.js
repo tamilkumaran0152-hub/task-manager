9ef7d1e81ebfb9b0d8dccf11045c637c01477d66
G Tamilkumaran <tamilkumaran0152@gmail.com>
2026-08-30 15:53:09 +0530

    Use environment port for deployment

--- backend/server.js
+++ backend/server.js
@@ -97,6 +97,8 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-    console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
 });
